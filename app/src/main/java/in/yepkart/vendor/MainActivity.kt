934c776f5e272c23398082c2cbe2e45e24d6b510
Abhrajit <aztech.it.system@gmail.com>
2022-02-14 13:53:28 +0530

    Commit Now

--- app/src/main/java/in/yepkart/vendor/MainActivity.kt
+++ app/src/main/java/in/yepkart/vendor/MainActivity.kt
@@ -1,16 +1,11 @@
 package `in`.yepkart.vendor
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SplashActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        var page = Intent(this, MainActivity::class.java)
-        startActivity(page)
-
-        Thread.sleep(2000)
+        setContentView(R.layout.activity_main)
     }
 }
