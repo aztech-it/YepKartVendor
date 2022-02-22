8fc776765ee4dc3072cd735b42088ce0847a79af
Abhrajit <aztech.it.system@gmail.com>
2022-02-22 14:55:37 +0530

    Commit

--- app/src/main/java/in/yepkart/vendor/MainActivity.kt
+++ app/src/main/java/in/yepkart/vendor/MainActivity.kt
@@ -2,14 +2,16 @@ package `in`.yepkart.vendor
 
 import `in`.yepkart.vendor.databinding.ActivityMainBinding
 import android.os.Bundle
-import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -23,9 +25,16 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        mDrawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        mDrawerLayout = findViewById(R.id.drawer_layout)
 
         binding.appBarMain.toolbar.navigationIcon = AppCompatResources.getDrawable(this, R.drawable.ic_hamburger)
+
+        supportActionBar!!.title = "Welcome User"
+        supportActionBar!!.subtitle = "Administrator"
+
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+
+        navView.setNavigationItemSelectedListener(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -35,7 +44,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == android.R.id.home) {
-            mDrawerLayout!!.openDrawer(Gravity.START)
+            mDrawerLayout!!.openDrawer(GravityCompat.START)
+        }
+
+        return true
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        mDrawerLayout!!.closeDrawer(GravityCompat.START)
+
+        when (item.itemId) {
+            R.id.nav_home -> {
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_gallery -> {
+                Toast.makeText(this, "Gallery", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_slideshow -> {
+                Toast.makeText(this, "Slideshow", Toast.LENGTH_SHORT).show()
+            }
         }
 
         return true
